c2bc407f6ae67e9f24e2e252c16e17f275ef67fd
Christoph Thelen <commana@gmx.de>
2020-03-03 19:14:04 +0100

    Spring Initializer

---
+++ src/main/java/de/techmastery/gaming/pubganalyzerbackend/PubgAnalyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.techmastery.gaming.pubganalyzerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubgAnalyzerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubgAnalyzerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/techmastery/gaming/pubganalyzerbackend/PubgAnalyzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.techmastery.gaming.pubganalyzerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubgAnalyzerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
