823326132c0c2b76253f5a875d4b895c61b1f481
Christoph Thelen <commana@gmx.de>
2020-03-05 13:59:10 +0100

    Add CORS support for local development

--- src/main/java/de/techmastery/gaming/pubganalyzerbackend/MatchesController.java
+++ src/main/java/de/techmastery/gaming/pubganalyzerbackend/MatchesController.java
@@ -1,5 +1,6 @@
 package de.techmastery.gaming.pubganalyzerbackend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MatchesController {
 
+    @CrossOrigin(origins = {"http://localhost:3000", "http://10.20.4.221:3000"})
     @GetMapping("/matches/{platform}/{player}")
     public Matches getMatchesForPlayer(@PathVariable("platform") String platform, @PathVariable("player") String player) {
         return new Matches(String.format("Hello, %2$s @ %1$s", platform, player));
